a1fc6b85c9ad6da8b207559d444cae17798d9a3c
g451 <g451@gmail.com>
2024-07-31 15:06:34 -0300

    aesthetic

--- frontend/src/componentes/Inicio.jsx
+++ frontend/src/componentes/Inicio.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 function Inicio(){
     const navigate = useNavigate()
-    const {register, handleSubmit, formState: {errors}} = useForm()
+    const {register, handleSubmit, formState: {errors}, setValue} = useForm()
 
     function verSimulacion(cantidadLineasASimular,duracionSimulacion,lineaInicioVisualizacion, lineaFinVisualizacion, cantidadSurtidores, cantidadEmpleadosGomeria, 
         cantidadEmpleadosVentaAccesorios, llegadaClientesMedia, llegadaClientesDesviacion, aDuracionCargaCombustible, bDuracionCargaCombustible,
@@ -25,11 +25,14 @@ function Inicio(){
         <>
         <form onSubmit={handleSubmit(onSubmit)}>
         <div className="row mt-3">
-            <h1>nombre tema final</h1>
+            <h1>Simulacion Estacion de Servicio</h1>
+            <div className="row mt-4 justify-content-center">
+            <button type="submit" className="btn btn-success col-1">Simular</button>
+        </div>
         </div>
 
         <div className="row text-center mt-5">
-            <label className="col-3 h5"> cantidadLineasASimular: </label>
+            <label className="col-3 h5"> Cantidad de lineas a Simular: </label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('cantidadLineasASimular', {required: 'El cantidadLineasASimular es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.cantidadLineasASimular && <span className='text-danger'>{errors.cantidadLineasASimular.message}</span>}
@@ -37,7 +40,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> duracionSimulacion:</label>
+            <label className="col-3 h5"> Duracion de la simulacion:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('duracionSimulacion', {required: 'duracionSimulacion es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.duracionSimulacion && <span className="text-danger">{errors.duracionSimulacion.message}</span>}
@@ -46,7 +49,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> lineaInicioVisualizacion:</label>
+            <label className="col-3 h5"> Numero de linea de inicio de visualizacion:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('lineaInicioVisualizacion', {required: 'lineaInicioVisualizacion es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.lineaInicioVisualizacion && <span className="text-danger">{errors.lineaInicioVisualizacion.message}</span>}
@@ -55,7 +58,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> lineaFinVisualizacion:</label>
+            <label className="col-3 h5"> Numero de linea de fin de visualizacion:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('lineaFinVisualizacion', {required: 'lineaFinVisualizacion es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.lineaFinVisualizacion && <span className="text-danger">{errors.lineaFinVisualizacion.message}</span>}
@@ -64,7 +67,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> cantidadSurtidores:</label>
+            <label className="col-3 h5"> Cantidad de surtidores:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('cantidadSurtidores', {required: 'cantidadSurtidores es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.cantidadSurtidores && <span className="text-danger">{errors.cantidadSurtidores.message}</span>}
@@ -73,7 +76,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> cantidadEmpleadosGomeria:</label>
+            <label className="col-3 h5"> Cantidad de empleados en gomeria:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('cantidadEmpleadosGomeria', {required: 'cantidadEmpleadosGomeria es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.cantidadEmpleadosGomeria && <span className="text-danger">{errors.cantidadEmpleadosGomeria.message}</span>}
@@ -81,7 +84,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> cantidadEmpleadosVentaAccesorios:</label>
+            <label className="col-3 h5"> Cantidad de empleados en venta de accesorios:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('cantidadEmpleadosVentaAccesorios', {required: 'cantidadEmpleadosVentaAccesorios es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.cantidadEmpleadosVentaAccesorios && <span className="text-danger">{errors.cantidadEmpleadosVentaAccesorios.message}</span>}
@@ -89,7 +92,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> llegadaClientesMedia:</label>
+            <label className="col-3 h5"> Media de la llegada de clientes:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('llegadaClientesMedia', {required: 'llegadaClientesMedia es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.llegadaClientesMedia && <span className="text-danger">{errors.llegadaClientesMedia.message}</span>}
@@ -97,15 +100,16 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> llegadaClientesDesviacion:</label>
+            <label className="col-3 h5"> Desviacion de la llegada de clientes:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('llegadaClientesDesviacion', {required: 'llegadaClientesDesviacion es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.llegadaClientesDesviacion && <span className="text-danger">{errors.llegadaClientesDesviacion.message}</span>}
             </div>
             
         </div>
+        <h7> Los siguientes inputs son para las distribuciones uniformes de los fines de atencion</h7>
         <div className="row mt-3">
-            <label className="col-3 h5"> aDuracionCargaCombustible:</label>
+            <label className="col-3 h5"> "A" Minima duracion en carga de combustible:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('aDuracionCargaCombustible', {required: 'aDuracionCargaCombustible es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.aDuracionCargaCombustible && <span className="text-danger">{errors.aDuracionCargaCombustible.message}</span>}
@@ -113,7 +117,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> bDuracionCargaCombustible:</label>
+            <label className="col-3 h5"> "B" Maxima duracion en carga de combustible:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('bDuracionCargaCombustible', {required: 'bDuracionCargaCombustible es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.bDuracionCargaCombustible && <span className="text-danger">{errors.bDuracionCargaCombustible.message}</span>}
@@ -121,7 +125,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> aDuracionAtGomeria:</label>
+            <label className="col-3 h5"> "A" Minima duracion en atencion de la gomeria:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('aDuracionAtGomeria', {required: 'aDuracionAtGomeria es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.aDuracionAtGomeria && <span className="text-danger">{errors.aDuracionAtGomeria.message}</span>}
@@ -129,7 +133,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> bDuracionAtGomeria:</label>
+            <label className="col-3 h5"> "B" Maxima duracion en atencion de la gomeria:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('bDuracionAtGomeria', {required: 'bDuracionAtGomeria es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.bDuracionAtGomeria && <span className="text-danger">{errors.bDuracionAtGomeria.message}</span>}
@@ -138,7 +142,7 @@ function Inicio(){
         </div>
 
         <div className="row mt-3">
-            <label className="col-3 h5"> aDuracionVentaAccesorios:</label>
+            <label className="col-3 h5"> "A" Minima duracion en venta accesorios:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('aDuracionVentaAccesorios', {required: 'aDuracionVentaAccesorios es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.aDuracionVentaAccesorios && <span className="text-danger">{errors.aDuracionVentaAccesorios.message}</span>}
@@ -146,7 +150,7 @@ function Inicio(){
             
         </div>
         <div className="row mt-3">
-            <label className="col-3 h5"> bDuracionVentaAccesorios:</label>
+            <label className="col-3 h5"> "B" Maxima duracion en venta accesorios:</label>
             <div className="col-5">
             <input type="text" className="form-control col-3 text-start" {...register('bDuracionVentaAccesorios', {required: 'bDuracionVentaAccesorios es requerido', validate: (value) => { return !isNaN(value) || 'Debe ser numérico' }})}></input>
             {errors.bDuracionVentaAccesorios && <span className="text-danger">{errors.bDuracionVentaAccesorios.message}</span>}
@@ -154,9 +158,7 @@ function Inicio(){
             
         </div>
 
-        <div className="row mt-4 justify-content-center">
-            <button type="submit" className="btn btn-success col-1">Simular</button>
-        </div>
+        
         
         </form>
         </>
